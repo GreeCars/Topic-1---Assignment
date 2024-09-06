881d656e768545131e2faadcac973d11a32c9a3a
Carsen Greenwood <362570749@GoTVDSB.ca>
2024-09-06 09:27:54 -0400

    Finished

--- Topic 1 - Assignment/Program.cs
+++ Topic 1 - Assignment/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Carsen Greenwood
+Console.WriteLine("Hello, my name is Carsen.");
+Console.WriteLine("");
+Console.WriteLine("Comments are typically used to leave messages to yourself or another programmer, or to prevent the execution of certain lines of code for testing purposes.");
+Console.WriteLine("");
+Console.WriteLine(" CCC   GGG ");
+Console.WriteLine("C   C G   G");
+Console.WriteLine("C     G    ");
+Console.WriteLine("C     GGGGG");
+Console.WriteLine("C     G   G");
+Console.WriteLine("C   C G   G");
+Console.WriteLine(" CCC   GGG ");
+// This is a comment
